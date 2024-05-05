a7fb2174fd9e39ceda5dd4dc727b12db566c578d
Aman Srivastava <amanrajasthan123@gmail.com>
2024-05-05 23:35:24 +0530

    Controller(Update user+  search user)

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,6 +3,36 @@ import mongoose from "mongoose";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helper/generateTokenAndSetCookie.js";
+
+export const getUserProfile = async (req, res) => {
+  // We will fetch user profile either with username or userId
+  // query is either username or userId
+  const { query } = req.params;
+
+  try {
+    let user;
+
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      // query is username
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in getUserProfile: ", err.message);
+  }
+};
+
 export const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -102,5 +132,35 @@ export const followUnfollowUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: err.message });
     console.log("Error in followUnfollowUser: ", err.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { name, email, username, password } = req.body;
+
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ error: "User not found" });
+
+    if (req.params.id !== userId.toString())
+      return res
+        .status(400)
+        .json({ error: "You cannot update other user's profile" });
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+
+    user = await user.save(); // Save the updated user
+    res.status(200).json({ message: "Profile updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in updateUser: ", error.message);
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,19 @@
 import express from "express";
 import {
   followUnfollowUser,
+  getUserProfile,
   loginUser,
   logoutUser,
   signupUser,
+  updateUser,
 } from "../controller/userController.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 
 const router = express.Router();
-
+router.get("/profile/:query", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update/:id", protectRoute, updateUser);
 export default router;
