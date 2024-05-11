e8a36f8491ce15e37488ed586aa1953fefcb7a87
Aman Srivastava <amanrajasthan123@gmail.com>
2024-05-11 19:21:36 +0530

    routes updated

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,13 +3,14 @@ import { protectRoute } from "../middlewares/protectRoute.js";
 import {
   createPost,
   deletePost,
+  getFeedPosts,
   getPost,
   likeUnlikePost,
   replyToPost,
 } from "../controller/postController.js";
 const router = express.Router();
 
-router.feed("/feed", protectRoute, getFeedPosts);
+router.post("/feed", protectRoute, getFeedPosts);
 router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/delete/:id", protectRoute, deletePost);
