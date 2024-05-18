ebbc7828220a0a2e85d025d9a7e64df3a21034a5
Aman Srivastava <amanrajasthan123@gmail.com>
2024-05-19 00:53:22 +0530

    Create post functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
@@ -13,30 +13,48 @@ import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
 import LogoutButton from "./components/LogoutButton";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
+import CreatePost from "./components/CreatePost";
 function App() {
-  const [count, setCount] = useState(0);
   const user = useRecoilValue(userAtom);
+  const { pathname } = useLocation();
   return (
-    <Container maxW={"620px"}>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <HomePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={!user ? <AuthPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/update"
-          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
-        />
-        <Route path="/:username" element={<UserPage />} />
-        <Route path="/:username/post/:pid" element={<PostPage />} />
-      </Routes>
-      {user && <LogoutButton />}
-    </Container>
+    <Box position={"relative"} w="full">
+      <Container
+        maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}
+      >
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!user ? <AuthPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/update"
+            element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/:username"
+            element={
+              user ? (
+                <>
+                  <UserPage />
+                  <CreatePost />
+                </>
+              ) : (
+                <UserPage />
+              )
+            }
+          />
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+        </Routes>
+        {user && <LogoutButton />}
+        {user && <CreatePost />}
+      </Container>
+    </Box>
   );
 }
 
