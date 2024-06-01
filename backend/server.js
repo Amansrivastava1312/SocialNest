3f8c6a0450f83b4d12f0ce49cdc83810758fb96f
Aman Srivastava <amanrajasthan123@gmail.com>
2024-06-02 01:04:24 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 dotenv.config();
 
 connectDB(); // Connect to MongoDB
 
 // const app = express();
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 // Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -29,6 +30,15 @@ app.use(cookieParser()); // Parse cookies
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // react app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
